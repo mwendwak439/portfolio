ce5a832b5678ed7c37ca1eec1703f166df2d40d3
kimanzi <mwendwak439@gmail.com>
2026-04-19 21:40:58 +1400

    added content in home component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,29 @@
+import Photo from '../images/Developer Frontend Developer Pin.jpeg'
+import {Link}from 'react-router-dom'
 function Home(){
     return(
-
-<h1>home</h1>
+<div className="hero_con">
+    <div> 
+        <img src={Photo} alt="profile pic" height={200} />
+    </div>
+    <div>  
+        <div className='namesection'>
+            <h6>welcome to my portfolio</h6>
+            <h2>Hi I'm <br></br>
+            <span className='myname'>Kennedy Kimanzi</span> <br />
+            crafting digital experience</h2>
+            <h4>bio</h4>
+         </div>
+         <div className="paradiv">
+            <p>A junior web developer passionate about  building clean,responsive ,and user-friendly website. 
+                I love turning design ideas into functional web pages and solving  small problems with code. </p>
+        <button> <Link to='/contact'>Contant Me</Link></button>
+        </div>
+    </div> 
+    <div>
+       
+    </div>
+</div>
     )
 }
 export default Home
